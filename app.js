a2edab333745504725426f3b07d95239c1d624da
amitvermaio <amitvns159@gmail.com>
2025-12-14 00:17:19 +0530

    feat: added express-rate-limit for basic api rate limiting.

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(
   })
 );
 
-app.use(rateLimit());
+app.use(rateLimit);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(join(__dirname, "public")));
